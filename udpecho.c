2d0de89bf0f7ad27071edf0fbe7909104b005410
ie702272 <ie702272@iteso.mx>
2018-04-27 01:19:48 -0500

    ImplementacionDMA

--- udpecho.c
+++ udpecho.c
@@ -42,54 +42,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#include "fsl_edma.h"
-#include "fsl_dmamux.h"
-
-#include "pin_mux.h"
-#include "clock_config.h"
-
-
-#define EXAMPLE_DMA DMA0
-#define EXAMPLE_DMAMUX DMAMUX0
-#define DEMO_DMAMUX_BASEADDR DMAMUX
-
-/*******************************************************************************
- * Variables
- ******************************************************************************/
-edma_handle_t g_EDMA_Handle;             /* Edma handler */
-edma_transfer_config_t g_transferConfig; /* Edma transfer config. */
-
-edma_transfer_config_t transferConfig;
-
-volatile bool g_Transfer_Done = false;
-volatile uint32_t g_index = 0U;          /* Index of the g_dacDataArray array. */
-static char songVal[9146];			 /* Array containing song values */
-uint32_t srcAdress = 0x200098a2;
-/*******************************************************************************
- * Code
- ******************************************************************************/
-
-/* User callback function for EDMA transfer. */
-void EDMA_Callback(edma_handle_t *handle, void *param, bool transferDone, uint32_t tcds)
-{
-	/* Clear Edma interrupt flag. */
-    EDMA_ClearChannelStatusFlags(EXAMPLE_DMA, 0U, kEDMA_InterruptFlag);
-    /* Setup transfer */
-    g_index += DAC_DATL_COUNT;
-    srcAdress = srcAdress + 0x04;
-    if (g_index == 9146)
-       {
-           g_index = 0U;
-           srcAdress = 0x200098a2;
-       }
-    EDMA_PrepareTransfer(&transferConfig, (void *)srcAdress, sizeof(uint16_t), (void *)(songVal + g_index), sizeof(songVal[0]),
-    			sizeof(songVal[0]), sizeof(songVal), kEDMA_MemoryToMemory);
-
-    EDMA_SetTransferConfig(EXAMPLE_DMA, 0U, &g_transferConfig, NULL);
-    /* Enable transfer. */
-    EDMA_StartTransfer(&g_EDMA_Handle);
-}
-
 static void
 server_thread(void *arg)
 {
@@ -103,17 +55,40 @@ server_thread(void *arg)
 	LWIP_UNUSED_ARG(arg);
 	conn = netconn_new(NETCONN_UDP);
 	netconn_bind(conn, IP_ADDR_ANY, 50007);
-    EDMA_SubmitTransfer(&g_EDMA_Handle, &transferConfig);
 	//LWIP_ERROR("udpecho: invalid conn", (conn != NULL), return;);
+	uint16_t total = 0;
+	uint16_t *p;
+	p = &total;
 
 	while (1)
 	{
 		netconn_recv(conn, &buf);
 		netbuf_data(buf, (void**)&msg, &len);
-		EDMA_StartTransfer(&g_EDMA_Handle);
-		/* Wait for EDMA transfer finish */
+
+		total = 0;
+		uint16_t val_0 = *(msg) -48;
+		uint16_t val_1 = *(msg+1) -48;
+		uint16_t val_2 = *(msg+2) -48;
+		uint16_t val_3 = *(msg+3) -48;
+		uint16_t val_4 = *(msg+4) -48;
+
+		if(val_0<10)
+			total = val_0 + (total*10);
+		if(val_1<10)
+			total = val_1 + (total*10);
+		if(val_2<10)
+			total = val_2 + (total*10);
+		if(val_3<10)
+			total = val_3 + (total*10);
+		if(val_4<10)
+			total = val_4 + (total*10);
+
+		float totalf = (float) total/32797;
+
+		totalf += 1;
 
 		netbuf_delete(buf);
+
 	}
 }
 
@@ -147,23 +122,6 @@ udpecho_init(void)
 {
 //	sys_thread_new("client", client_thread, NULL, 300, 1);
 	sys_thread_new("server", server_thread, NULL, 300, 2);
-    edma_config_t userConfig;
-
-    DMAMUX_Init(EXAMPLE_DMAMUX);
-#if defined(FSL_FEATURE_DMAMUX_HAS_A_ON) && FSL_FEATURE_DMAMUX_HAS_A_ON
-    DMAMUX_EnableAlwaysOn(EXAMPLE_DMAMUX, 0, true);
-#else
-    DMAMUX_SetSource(EXAMPLE_DMAMUX, 0, 63);
-#endif /* FSL_FEATURE_DMAMUX_HAS_A_ON */
-    DMAMUX_EnableChannel(EXAMPLE_DMAMUX, 0);
-
-    EDMA_GetDefaultConfig(&userConfig);
-	EDMA_Init(EXAMPLE_DMA, &userConfig);
-	EDMA_CreateHandle(&g_EDMA_Handle, EXAMPLE_DMA, 0);
-	EDMA_SetCallback(&g_EDMA_Handle, EDMA_Callback, NULL);
-	EDMA_PrepareTransfer(&transferConfig, (void *)0x200098a2, sizeof(uint16_t), (void *)(songVal + g_index), sizeof(songVal[0]),
-						 sizeof(songVal[0]), sizeof(songVal), kEDMA_MemoryToMemory);
-
 
 }
 
