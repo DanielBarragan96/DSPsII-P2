978a4e312a7162271c0fbe277576d0d61de2921e
ie702272 <ie702272@iteso.mx>
2018-04-29 10:26:18 -0500

    Cancion
    Audio con mucho ruido

--- lwip_udpecho_freertos.c
+++ lwip_udpecho_freertos.c
@@ -44,12 +44,13 @@
 #include "lwip/tcpip.h"
 #include "netif/ethernet.h"
 #include "ethernetif.h"
-
+#include "fsl_pit.h"
 #include "board.h"
 
 #include "fsl_device_registers.h"
 #include "pin_mux.h"
 #include "clock_config.h"
+#include "fsl_dac.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -103,6 +104,21 @@
 /*!
  * @brief Initializes lwIP stack.
  */
+#define CLK_FREQ_HZ 50000000  /* CLKIN0 frequency */
+#define SLOW_IRC_FREQ 32768	/*This is the approximate value for the slow irc*/
+#define FAST_IRC_FREQ 4000000 /*This is the approximate value for the fast irc*/
+#define EXTERNAL_CLOCK 0 /*It defines an external clock*/
+#define PLL_ENABLE 1 /**PLL is enabled*/
+#define PLL_DISABLE 0 /**PLL is disabled*/
+#define CRYSTAL_OSC 1  /*It defines an crystal oscillator*/
+#define LOW_POWER 0     /* Set the oscillator for low power mode */
+#define SLOW_IRC 0 		/* Set the slow IRC */
+#define CLK0_TYPE 0     /* Crystal or canned oscillator clock input */
+#define PLL0_PRDIV 25    /* PLL predivider value */
+#define PLL0_VDIV 50    /* PLL multiplier value*/
+#define PLL_DIRECT_INIT
+
+
 static void stack_init(void *arg)
 {
     static struct netif fsl_netif0;
@@ -140,11 +156,42 @@ static void stack_init(void *arg)
     vTaskDelete(NULL);
 }
 
+static void DAC_ADC_Init(void)
+{
+    dac_config_t dacConfigStruct;
+
+    /* Configure the DAC. */
+    /*
+     * dacConfigStruct.referenceVoltageSource = kDAC_ReferenceVoltageSourceVref2;
+     * dacConfigStruct.enableLowPowerMode = false;
+     */
+    DAC_GetDefaultConfig(&dacConfigStruct);
+    DAC_Init(DAC0, &dacConfigStruct);
+    DAC_Enable(DAC0, true); /* Enable output. */
+}
+
 /*!
  * @brief Main function
  */
 int main(void)
 {
+	//Change the Kinetis clock speed
+	 int mcg_clk_hz;
+
+	 unsigned char modeMCG = 0;
+
+
+		#ifndef PLL_DIRECT_INIT
+		   mcg_clk_hz = fei_fbi(SLOW_IRC_FREQ,SLOW_IRC);// 64 Hz ---> 32768
+		   mcg_clk_hz = fbi_fbe(CLK_FREQ_HZ,LOW_POWER,EXTERNAL_CLOCK); // 97.656KHz ---> 50000000
+		   mcg_clk_hz = fbe_pbe(CLK_FREQ_HZ,PLL0_PRDIV,PLL0_VDIV);	// 97.656KHz ---> 50000000 and PLL is configured to generate 60000000
+		   mcg_clk_hz =  pbe_pee(CLK_FREQ_HZ);// 117.18 KHz ---> 60000000
+		#else
+		      mcg_clk_hz = pll_init(CLK_FREQ_HZ, LOW_POWER, EXTERNAL_CLOCK, PLL0_PRDIV, PLL0_VDIV, PLL_ENABLE);
+		#endif
+
+	   modeMCG = what_mcg_mode();
+
     SYSMPU_Type *base = SYSMPU;
     BOARD_InitPins();
     BOARD_BootClockRUN();
@@ -152,9 +199,21 @@ int main(void)
     /* Disable SYSMPU. */
     base->CESR &= ~SYSMPU_CESR_VLD_MASK;
 
+    pit_config_t pit_config = {	true };
+
+    PIT_Init(PIT, &pit_config);
+
+	PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+	EnableIRQ(PIT0_IRQn);
+
+	PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, USEC_TO_COUNT(105, CLOCK_GetFreq(kCLOCK_BusClk)));
+
+
     /* Initialize lwIP from thread */
     if(sys_thread_new("main", stack_init, NULL, INIT_THREAD_STACKSIZE, INIT_THREAD_PRIO) == NULL)
         LWIP_ASSERT("main(): Task creation failed.", 0);
+
+    DAC_ADC_Init();
 
     vTaskStartScheduler();
 
---
+++ udpecho.c
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the lwIP TCP/IP stack.
+ * 
+ * Author: Adam Dunkels <adam@sics.se>
+ *
+ */
+
+#include "udpecho.h"
+#include "stdlib.h"
+#include "lwip/opt.h"
+#include "fsl_pit.h"
+#include "fsl_dac.h"
+
+#if LWIP_NETCONN
+
+#include "lwip/api.h"
+#include "lwip/sys.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define ARRAY_SIZE_BUF 225
+#define ARRAY_NUMBER 6
+
+uint8_t counter = 0;
+uint16_t newbuf[ARRAY_SIZE_BUF] = {0};
+uint16_t newbuf2[ARRAY_SIZE_BUF] = {0};
+
+bool pit_flag = false;
+
+void PIT0_IRQHandler()
+	{
+		PIT_ClearStatusFlags(PIT, kPIT_Chnl_0, kPIT_TimerFlag);
+
+		DAC_SetBufferValue(DAC0, 0U,(newbuf[counter]));
+		DAC_SetBufferValue(DAC0, 0U,(newbuf2[counter+1]));
+
+		counter = (counter < (ARRAY_SIZE_BUF  -ARRAY_NUMBER)) ? counter + 1 : 0;
+
+
+	}
+
+static void
+server_thread(void *arg)
+{
+	struct netconn *conn;
+	struct netbuf *buf;
+
+	char *msg;
+
+	uint16_t len;
+
+	LWIP_UNUSED_ARG(arg);
+	conn = netconn_new(NETCONN_UDP);
+	netconn_bind(conn, IP_ADDR_ANY, 50007);
+	//LWIP_ERROR("udpecho: invalid conn", (conn != NULL), return;);
+
+	while (1)
+	{
+		netconn_recv(conn, &buf);
+		netbuf_data(buf, (void**)&msg, &len);
+		//if(false == pit_flag)
+			netbuf_copy(buf, newbuf, sizeof(newbuf));
+			netbuf_copy(buf, newbuf2, sizeof(newbuf));
+
+
+		PIT_StartTimer(PIT, kPIT_Chnl_0);
+		pit_flag = (pit_flag == true)?false:true;
+		netbuf_delete(buf);
+
+	}
+}
+
+/*-----------------------------------------------------------------------------------*/
+void
+udpecho_init(void)
+{
+	sys_thread_new("server", server_thread, NULL, 300, 2);
+}
+
+#endif /* LWIP_NETCONN */
